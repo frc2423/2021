3c5cbf10e03689a48edce9dac353233d7b8c9c9c
Wheeleral <Awheeler2@wpi.edu>
2021-02-11 20:22:56 -0500

    More paths-henry

--- test/src/main/java/frc/robot/Robot.java
+++ test/src/main/java/frc/robot/Robot.java
@@ -30,7 +30,7 @@ public class Robot extends TimedRobot {
 
   private TrajectoryFollower follower;
     
-  String trajectoryJSON = "GalacticSearchRedB";
+  String trajectoryJSON = "GalacticSearchRedA";
 
   /**
    * This function is run when the robot is first started up and should be used for any

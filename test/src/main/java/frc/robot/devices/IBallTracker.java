51c2e2bb34ea21ab24b3a92d495764450ba5f4e6
Henry Yusem <hyusem@gmail.com>
2021-02-21 15:29:16 -0500

    vision files

---
+++ test/src/main/java/frc/robot/devices/IBallTracker.java
@@ -0,0 +1,21 @@
+package frc.robot.devices;
+
+import org.photonvision.PhotonTrackedTarget;
+
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+
+public interface IBallTracker {
+    
+    public boolean hasTargets();
+
+    public PhotonTrackedTarget getBestTarget();
+
+    public double getDistanceFromTarget();
+
+    public double getAngleFromTarget();
+
+    public void addSimulatedBall(double x, double y);
+
+    public void giveRobotPose(Pose2d pose);
+
+}

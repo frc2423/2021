04e37295c88b410e59c26b81af104b56492c18bd
unknown <bulworm@gmail.com>
2021-02-22 18:39:17 -0500

    SimBallTracker & BallTracker created

--- test/src/main/java/frc/robot/devices/SimBallTracker.java
+++ test/src/main/java/frc/robot/devices/SimBallTracker.java
@@ -3,16 +3,23 @@ package frc.robot.devices;
 import org.photonvision.PhotonTrackedTarget;
 import org.photonvision.SimVisionTarget;
 import org.photonvision.SimVisionSystem;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
+
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.util.Units;
 import edu.wpi.first.wpilibj.geometry.Transform2d;
 
-public class SimBallTracker {
+public class SimBallTracker implements IBallTracker{
     
         // Simulated Vision System.
     // Configure these to match your PhotonVision Camera,
     // pipeline, and LED setup.
+    PhotonCamera camera = new PhotonCamera("photonvision");
     double camDiagFOV = 170.0; // degrees - assume wide-angle camera
-    double camPitch = 0; //Units.radiansToDegrees(Robot.CAMERA_PITCH_RADIANS); // degrees
-    double camHeightOffGround = 0; //Robot.CAMERA_HEIGHT_METERS; // meters
+    double camPitch =  Units.degreesToRadians(15); //Units.radiansToDegrees(Robot.CAMERA_PITCH_RADIANS); // degrees // tbd
+    double camHeightOffGround = .5; //Robot.CAMERA_HEIGHT_METERS; // meters // tbd
     double maxLEDRange = 20; // meters
     int camResolutionWidth = 640; // pixels
     int camResolutionHeight = 480; // pixels
@@ -30,9 +37,34 @@ public class SimBallTracker {
                     camResolutionHeight,
                     minTargetArea);
 
-    private void simFunction() {
-         //simVision.addSimVisionTarget(
-        //         new SimVisionTarget(farTargetPose, Robot.TARGET_HEIGHT_METERS, targetWidth, targetHeight));
+    public boolean hasTargets(){
+        return camera.getLatestResult().hasTargets();
+    }
+
+    public PhotonTrackedTarget getBestTarget(){
+        return camera.getLatestResult().getBestTarget();
+    }
+
+    public double getDistanceFromTarget(){ // uhhhhhh
+        return PhotonUtils.calculateDistanceToTargetMeters(
+            camHeightOffGround,
+            0,
+            camPitch,
+            Units.degreesToRadians(camera.getLatestResult().getBestTarget().getPitch()));
+
+    }
+
+    public double getAngleFromTarget(){
+        return camera.getLatestResult().getBestTarget().getYaw();
+    }
+
+    public void addSimulatedBall(double x, double y){
+        simVision.addSimVisionTarget(
+            new SimVisionTarget(new Pose2d(x, y, new Rotation2d()), 0, 27, 27));
+    }
+
+    public void giveRobotPose(Pose2d pose){
+        simVision.processFrame(pose);
     }
 
 }

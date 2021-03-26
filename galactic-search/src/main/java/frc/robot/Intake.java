b3484b9acb654e601c05739ac3a71cdb2d3b7783
Henry Yusem <hyusem@gmail.com>
2021-03-26 17:04:44 -0400

    copying basic files over

---
+++ galactic-search/src/main/java/frc/robot/Intake.java
@@ -0,0 +1,118 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.devices.IMotor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
+public class Intake {
+    enum State{
+        INTAKEBALLS, OUTTAKE, STALLED, NOTHING
+    }
+  
+    private State state = State.NOTHING;
+    private Timer timer = new Timer();
+    private double zoom  = 0.6;
+    private IMotor motor;
+    private Boolean running;
+    private double stallZone = 1500.0;
+    private DoubleSolenoid intakeValve;
+    private IMotor greenWheel;
+
+    private double motorSpeed = 0.0;
+    private double greenWheelSpeed = 0.0;
+    private DoubleSolenoid.Value intakeValue = DoubleSolenoid.Value.kOff;
+
+    public void init() {
+        running = false;
+        intakeValve = new DoubleSolenoid(2, 3);
+        motor = Manager.getDevice("intakeMotor", IMotor.class);
+        greenWheel = Manager.getDevice("greenWheel", IMotor.class);
+        stop();
+    }
+
+    public void stop (){
+        running = false;
+        runIntake();
+        greenWheelSpeed = 0.0;
+    }
+
+    public void intake() {
+        running = true;
+        runIntake();
+        greenWheelSpeed = -.3;
+    }
+
+    public void turnWheel(double speed) {
+        greenWheelSpeed = speed;
+    }
+
+    public boolean isDown() {
+        return intakeValve.get() == DoubleSolenoid.Value.kReverse;
+    }
+    public void intakeUp(){
+        intakeValue = DoubleSolenoid.Value.kForward;
+    }
+    public void intakeDown(){
+        intakeValue = DoubleSolenoid.Value.kReverse;
+    }
+   
+    private void runIntake(){
+        switch(state){
+            case NOTHING: 
+              motorSpeed = 0.0;
+              if(running){
+                state = State.INTAKEBALLS;
+              }
+              break;
+            case INTAKEBALLS :
+                motorSpeed = zoom;
+                if(motor.getSpeed() > -stallZone && motor.getSpeed() < stallZone){
+                  state = State.STALLED;
+                  timer.stop();
+                  timer.reset();
+                  timer.start();
+                }
+                else if (!running){
+                  state = State.NOTHING;
+                }
+                break;
+            case STALLED :
+                motorSpeed = zoom;
+                if(motor.getSpeed() < -stallZone && motor.getSpeed() > stallZone){
+                  state = State.INTAKEBALLS;
+                  timer.stop();
+                  timer.reset();
+                  timer.start();
+                }
+                else if(timer.get()>.5){
+                  state = State.OUTTAKE;
+                  timer.stop();
+                  timer.reset();
+                  timer.start();
+                }
+                else if (!running){
+                  state = State.NOTHING;
+                }
+                break;
+            case OUTTAKE :
+                motorSpeed = -zoom;
+                if(timer.get() > 1){
+                  state = State.INTAKEBALLS;
+                  timer.stop();
+                  timer.reset();
+                  timer.start();
+                }
+                else if(!running){
+                  state = State.NOTHING;
+                }
+                break;
+        }
+    }
+
+    public void execute() {
+        motor.setPercent(motorSpeed);
+        greenWheel.setPercent(greenWheelSpeed);
+        intakeValve.set(intakeValue);
+    }
+  
+}
